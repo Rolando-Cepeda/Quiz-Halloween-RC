b352054cbe3e61971ec336a57d8308d024e61853
Rolando_cf <125164239+Rolando-Cepeda@users.noreply.github.com>
2024-11-18 20:54:01 +0100

    Revisar, salen los datos en console log, pero no en el HTML

--- Halloween _2.js
+++ Halloween _2.js
@@ -2,10 +2,26 @@
 	localStorage.clear();
 })();
 
-//Función para crear las preguntas
+// 1.- Creamos una función para traer las preguntas de la API
+const answer = function () {
+
+	fetch(
+		"https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple"
+
+	)
+		.then((response) => response.json())
+		.then((preguntas) => crearPreguntas(preguntas))
+		//console.log(preguntas.results[1])
+
+		.catch((error) => console.error("Error al obtener las preguntas:", error));
+};
+
+
+//2.- Función para crear las preguntas
 const crearPreguntas = (preguntas) => {
 	const listaPreguntas = preguntas.results;
 
+	//2.1.- Aquí recorre los resultados (fetch), trayéndome solo la QUESTION, CORRECT_ANSWER y INCORRECT_ANSWER.
 	const preguntasInfoTotal = listaPreguntas.map((pregunta) => {
 		return {
 			pregunta: pregunta.question,
@@ -13,17 +29,19 @@ const crearPreguntas = (preguntas) => {
 			repuestasIncorrectas: pregunta.incorrect_answers
 		};
 	});
-	//console.log(preguntasInfoTotal);
 
-	// Guardo las preguntas en el localstore
+
+	// 4.- Guardo las preguntas en el localstore
 	localStorage.setItem("preguntas", JSON.stringify(preguntasInfoTotal));
 
-	//Accedemos al contenedor principal y lo limpiamos
+	//5.- Accedemos al contenedor principal y lo limpiamos
 	const contenedor = document.getElementById("contenedor_preguntas");
 	contenedor.innerHTML = "";
 
 
 	preguntasInfoTotal.forEach((preguntaObj, index) => {
+		console.log(preguntasInfoTotal);
+		
 		const divPregunta = document.createElement("div");
 		divPregunta.className = "pregunta";
 
@@ -43,21 +61,22 @@ const crearPreguntas = (preguntas) => {
 			const botonRespuesta = document.createElement("button");
 			botonRespuesta.textContent = respuesta;
 			botonRespuesta.className = "opcion";
+
+			// Agregar evento al botón
+            botonRespuesta.addEventListener("click", () => {
+                if (respuesta === preguntaObj.respuestaCorrecta) {
+                    botonRespuesta.style.backgroundColor = "green";
+                } else {
+                    botonRespuesta.style.backgroundColor = "red";
+                }
+            });
+
+            divPregunta.appendChild(botonRespuesta);
+        });
+
+        contenedor.appendChild(divPregunta);
 		});
-	})
 
 }
 
 
-// Creamos una función para traer las preguntas de la API
-const answer = function () {
-
-	fetch(
-		"https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple"
-	)
-		.then((response) => response.json())
-		.then((preguntas) => crearPreguntas(preguntas))
-		//console.log(preguntas.results[1])
-
-		.catch((error) => console.error("Error al obtener las preguntas:", error));
-};
